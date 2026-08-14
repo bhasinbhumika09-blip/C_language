46362666aebad947152b06e7cb4702bfdea1f0bc
Bhumika Bhasin <bhasinbhumika09@gmail.com>
2026-08-15 01:10:23 +0530

    Create escape_sequence_character.c

---
+++ escape_sequence_character.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    printf("Heyyy!!! I am good \n nice \n");
+    printf("Heyyy!!! I am good \t nice \n");
+    printf("Heyyy!!! I am good \" nice \n");
+    printf("Heyyy!!! I am good \' nice \n");
+    printf("Heyyy!!! I am good \\ nice \n");
+    return 0;
+}

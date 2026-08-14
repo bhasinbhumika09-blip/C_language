5856267e57341a3deee1cc61cefea8072b76dbfe
Bhumika Bhasin <bhasinbhumika09@gmail.com>
2026-08-14 23:30:24 +0530

    Create print_statement.c

---
+++ print_statement.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+
+    printf("Hello everyone!!!");
+    printf("Myself Bhumika Bhasin.");
+    printf("Spiderman");
+
+
+//This helps us to print the statements but to make it look beautify and readable we use \n to give a new line.
+
+    printf("\nHello everyone!!! \n");
+    printf("Myself Bhumika Bhasin. \n");
+    printf("Spiderman \n");
+
+  return 0;
+}

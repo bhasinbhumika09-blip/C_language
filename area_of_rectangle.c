772d0249ba7165d9765b85e98ab69bc5d33b25b8
Bhumika Bhasin <bhasinbhumika09@gmail.com>
2026-08-15 01:24:17 +0530

    Create area_of_rectangle.c

---
+++ area_of_rectangle.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int length, breadth;
+
+    printf("Length is: ");
+    scanf("%d", &length);
+
+    printf("Breadth is: ");
+    scanf("%d", &breadth);
+
+    printf("The area of rectangle is %d", length * breadth);
+
+    return 0;
+}

bb9c59b6a89a6b831eb0af5d9e5a1bab82b2992a
Bhumika Bhasin <bhasinbhumika09@gmail.com>
2026-08-15 00:48:31 +0530

    Create datatypes.c

---
+++ datatypes.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+
+   int a=9;
+   printf("%d \n",a);
+
+   int b;
+   b=3;
+   printf("%d \n",b);
+
+   float c=6.7;
+   printf("%f \n",c);
+   
+   char d='g';
+   printf("%c \n",d);
+
+   char h[]="Bhumika";
+   printf("%s \n",h);
+
+  return 0;
+}

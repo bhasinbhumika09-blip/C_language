69ffa1e4860b3c523457bacd672e7620393db42b
Bhumika Bhasin <bhasinbhumika09@gmail.com>
2026-08-21 16:04:51 +0530

    Update and rename &&.c to &&_||.c

--- &&_||.c
+++ &&_||.c
@@ -12,15 +12,21 @@ int main() {
     scanf("%d",&c);
 
     // && means both conditions must be true
+    // || means one of the two conditions must be true
+    // more statement cases can also be made but for now its it
     
     if(a>b && a>c){
-        printf("a is greater than b and c.");
+        printf("a is greater than b and c.\n");
     }
     else if(b>a && b>c){
-        printf("b is greater than a and c.");
+        printf("b is greater than a and c.\n");
+    }
+    else if((a==b) || (a==c)){
+        printf("a is either equal to b or c. \n");
     }
     else{
-        printf("c is greater than a and b.");
+        printf("c is greater than a and b.\n");
     }
+
     return 0;
 }

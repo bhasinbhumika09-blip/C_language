7b6c8bbb25f513114c637539abe76c19b724f6f0
Bhumika Bhasin <bhasinbhumika09@gmail.com>
2026-08-21 16:17:48 +0530

    Create _003.c

---
+++ _003.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    
+    printf("Enter a munber:");
+    scanf("%d",&a);    
+
+    b=a+9;
+    printf("value of a is %d and value of b is %d.",a,b);
+    return 0;
+}

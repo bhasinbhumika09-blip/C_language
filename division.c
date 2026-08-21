d23d413f34932bb3e21034ef82912a8947306241
Bhumika Bhasin <bhasinbhumika09@gmail.com>
2026-08-21 15:56:58 +0530

    Add division program to demonstrate int and float division

---
+++ division.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int a=5,b=2,c=a/b;
+    float d=(float)a/b;
+    printf("int division is %d \n",c);
+    printf("float division is %f \n",d);
+    return 0;
+}
